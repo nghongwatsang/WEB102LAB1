a2824498c9519567a8293352812170bf64851c3f
Ngawang Ghongwatsang <nghongwatsang@gmail.com>
2025-03-01 14:57:52 -0500

    done

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>VACATION SCHEDULE</h1>
+      <h2>Plans for the family's week long trip to Spain</h2>
+      <Calendar />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import Event from "./Event";
+
+const Calendar = () => {
+    return(
+        <div className="Calendar"> 
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td className= 'Event' ></td>
+                            <Event event='Fancy Dinner 🎩' color ='green'/>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">8 am</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">9 am</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">10 am</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">11 am</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">12 pm</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">1 pm</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    <tbody>
+                        <tr>
+                            <td className="time">2 pm</td>
+                            <td>  </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody> 
+                    
+                </thead>
+            </table>
+        
+        </div>
+    )
+}
+
+
+export default Calendar
